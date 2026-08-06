3c6f880bc5aca3e9c984e180c5dfeacabfcad94b
Juan Colonese <Juan.Colonese@ecmwf.int>
2026-08-06 09:20:00 +0000

    fix: load tools first-thing

--- src/hyve_flow/reanalysis.py
+++ src/hyve_flow/reanalysis.py
@@ -30,8 +30,8 @@ class ReanalysisProcessing:
             name="extract_stations",
             variables={"WORKDIR": work_dir},
             script=[
-                *config_script(config=self.config, output_file="extract.yaml", work_dir=work_dir),
                 self.tools.load(self.exec_env),
+                *config_script(config=self.config, output_file="extract.yaml", work_dir=work_dir),
                 *script
             ],
             **task_args
