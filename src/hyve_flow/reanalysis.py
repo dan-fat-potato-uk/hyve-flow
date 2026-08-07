794c89060fde3da8bb8557486ac6a1fa29c0f77e
Daniel Foley <ecme8937@ac6-102.bullx>
2026-08-07 11:14:33 +0000

    Remove .get

--- src/hyve_flow/reanalysis.py
+++ src/hyve_flow/reanalysis.py
@@ -32,7 +32,7 @@ class ReanalysisProcessing:
             variables={"WORKDIR": work_dir},
             script=[
                 self.tools.load(self.exec_env),
-                *config_script(config=self.config.get("extraction"), output_file="extract.yaml", work_dir=work_dir),
+                *config_script(config=self.config.extraction, output_file="extract.yaml", work_dir=work_dir),
                 *script
             ],
             **task_args
